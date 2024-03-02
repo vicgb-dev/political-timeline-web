fcdfc19a7e0a766585bc548d52a8bb867aaeb486
VictorGarGen <93768447+vicgb-dev@users.noreply.github.com>
2024-03-02 22:34:20 +0100

    Uso de MockEvents

--- src/services/events-service.ts
+++ src/services/events-service.ts
@@ -4,25 +4,25 @@ import { PoliticalEvent } from '../models/event.interface'
 
 export class EventsService {
   static async getPublishedEvents (): Promise<PoliticalEvent[]> {
-    // return new Promise((resolve, reject) => {
-    //   setTimeout(() => {
-    //     resolve(fakeEvents)
-    //   }, 500)
-    // })
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(fakeEvents)
+      }, 500)
+    })
 
-    try {
-      const response = await fetch(`${HOST}${EVENT}${PUBLISHED}`)
-      if (!response.ok) {
-        throw new Error('Error al obtener los datos de la API')
-      }
-      let data: PoliticalEvent[] = await response.json()
-      data = data.sort((a, b) => {
-        return new Date(b.eventDate).getTime() - new Date(a.eventDate).getTime()
-      })
-      return data
-    } catch (error) {
-      console.error('Error en la llamada a la API:', error)
-      throw error
-    }
+    // try {
+    //   const response = await fetch(`${HOST}${EVENT}${PUBLISHED}`)
+    //   if (!response.ok) {
+    //     throw new Error('Error al obtener los datos de la API')
+    //   }
+    //   let data: PoliticalEvent[] = await response.json()
+    //   data = data.sort((a, b) => {
+    //     return new Date(b.eventDate).getTime() - new Date(a.eventDate).getTime()
+    //   })
+    //   return data
+    // } catch (error) {
+    //   console.error('Error en la llamada a la API:', error)
+    //   throw error
+    // }
   }
 }
